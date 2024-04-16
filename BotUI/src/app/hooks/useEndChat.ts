725e54f9011e92f6da7d777616832892786646a6
Shlomi Shitrit <akunh113@gmail.com>
2024-04-16 10:16:44 +0300

    adapt useEndChat hook for a general bot class

--- BotUI/src/app/hooks/useEndChat.ts
+++ BotUI/src/app/hooks/useEndChat.ts
@@ -1,8 +1,9 @@
 import { useRecoilState } from "recoil";
-import { messagesSectionAtom } from "@/app/store/atoms";
+import { messagesSectionAtom, botAtom } from "@/app/store/atoms";
 import {
     QueryWords,
-    Operator,
+    NumericOperator,
+    StringOperator,
     NumericAttribute,
     StringAttribute,
 } from "@/app/general/interfaces";
@@ -14,15 +15,14 @@ import { isNumberArray } from "@/app/general/utils";
 import { strParamType } from "@/app/general/types";
 
 export default function useEndChat(strParam: strParamType) {
+    const [bot, __] = useRecoilState(botAtom);
     const [messages, _] = useRecoilState(messagesSectionAtom);
+
     const handleEndChat = (): QueryWords => {
-        const wordsParams: QueryWords = {
-            age_of_aquisition: null,
-            number_of_phon: null,
-            number_of_syll: null,
-            start_with: null,
-            sound_like: null,
-        };
+        const wordsParams: QueryWords = {};
+        bot.columns.forEach((col) => {
+            wordsParams[col?.displayName] = null;
+        });
 
         const numericAttribute: NumericAttribute = {
             ...emptyNumericAttribute,
@@ -39,51 +39,44 @@ export default function useEndChat(strParam: strParamType) {
             userFilteredMessages?.forEach((msg) => {
                 switch (msg?.typeOfQuestion) {
                     case "value":
-                        if (!strParam.state) {
-                            if (numericAttribute.operator === Operator.Range) {
-                                if (isNumberArray(numericAttribute.value)) {
-                                    numericAttribute.value.push(
-                                        Number(msg?.text)
-                                    );
-                                }
-                            } else {
-                                numericAttribute.value = Number(msg?.text);
-                            }
-                        } else {
-                            stringAttribute.value = msg?.text;
-                        }
+                        const isRange =
+                            numericAttribute.operator === NumericOperator.Range;
+                        if (strParam.state) stringAttribute.value = msg?.text;
+                        if (!isRange)
+                            numericAttribute.value = Number(msg?.text);
+                        if (isNumberArray(numericAttribute.value))
+                            numericAttribute.value.push(Number(msg?.text));
+
                         break;
                     case "operator":
                         if (!strParam.state) {
                             numericAttribute.operator = [
-                                Operator.Greater,
-                                Operator.Lower,
-                                Operator.Range,
-                                Operator.Equal,
+                                NumericOperator.Greater,
+                                NumericOperator.Lower,
+                                NumericOperator.Range,
+                                NumericOperator.Equal,
                             ][Number(msg?.text) - 1];
                             if (Number(msg?.text) === 3) {
                                 numericAttribute.value = [] as number[];
                             }
                         } else {
-                            wordsParams[
-                                ["start_with", "sound_like"][
-                                    Number(msg?.text) - 1
-                                ] as keyof QueryWords
-                            ] = stringAttribute as StringAttribute &
-                                NumericAttribute;
+                            stringAttribute.operator = [
+                                StringOperator.StartWith,
+                                StringOperator.SoundLike,
+                            ][Number(msg?.text) - 1];
                         }
                         break;
                     case "parameter":
-                        if (!strParam.state) {
-                            wordsParams[
-                                [
-                                    "age_of_aquisition",
-                                    "number_of_phon",
-                                    "number_of_syll",
-                                ][Number(msg?.text) - 1] as keyof QueryWords
-                            ] = numericAttribute as NumericAttribute &
-                                StringAttribute;
-                        }
+                        const param = bot.headers[
+                            Number(msg?.text) - 1
+                        ] as keyof QueryWords;
+                        const paramColumn = bot.columns.find(
+                            (col) => col.displayName === param
+                        );
+                        wordsParams[param] =
+                            paramColumn?.dataType === "numeric"
+                                ? numericAttribute
+                                : stringAttribute;
                         break;
                     default:
                         break;
