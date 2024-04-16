645ae39cfa64dfb144dd10b18d3e326a7acc969b
Shlomi Shitrit <akunh113@gmail.com>
2024-04-16 10:14:26 +0300

    add comment for prod

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var _a = require("./DBbot"), DBbot = _a.DBbot, appClass = _a.app;
 var dbBot = new DBbot();
 var app = new appClass("./BotUI");
+// Testing
+// TODO: Remove this
 dbBot.loadFile("./data.csv");
 app.runBot(dbBot);
 module.exports = {
