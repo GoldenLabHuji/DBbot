4408318390788939a484601cf7f9600017f531ae
Shlomi Shitrit <akunh113@gmail.com>
2024-05-05 11:12:08 +0300

    split loafFile func to smaller funcs

--- DBbot/classes/DBbot.ts
+++ DBbot/classes/DBbot.ts
@@ -37,43 +37,50 @@ export class DBbot {
                 });
             }
 
-            rows.forEach((row: string) => {
-                const columns = row
-                    .split(",")
-                    .map((column: string) => column.trim());
-                columns.forEach((column: string, index: string | number) => {
-                    const columnName = this.headers[index as number];
-                    if (columnName && column !== "") {
-                        const columnData = this.dataMap.get(columnName);
-                        if (columnData) {
-                            columnData.push(column);
-                        }
-                    }
-                });
-            });
-
-            this.headers.forEach((column) => {
-                const columnData = this.dataMap.get(column);
-                if (columnData && columnData.length > 0) {
-                    const dataType: DataType = Number(columnData[0])
-                        ? "numeric"
-                        : "string";
-                    const col = new Column(column, dataType);
-                    const numberColumnData: number[] = columnData.map(
-                        (item: string) => parseFloat(item)
-                    );
-                    col.addRows(
-                        dataType === "numeric" ? numberColumnData : columnData
-                    );
-                    this.addColumn(col);
-                } else {
-                    console.error(
-                        `Column ${column} has no data. Skipping this column.`
-                    );
-                }
-            });
+            this.addColumsToDataMap(rows);
+            this.addColumnsAuto();
         } catch (error) {
             console.error(error);
         }
+    }
+
+    private addColumsToDataMap(rows: string[]): void {
+        rows.forEach((row: string) => {
+            const columns = row
+                .split(",")
+                .map((column: string) => column.trim());
+            columns.forEach((column: string, index: string | number) => {
+                const columnName = this.headers[index as number];
+                if (columnName && column !== "") {
+                    const columnData = this.dataMap.get(columnName);
+                    if (columnData) {
+                        columnData.push(column);
+                    }
+                }
+            });
+        });
+    }
+
+    private addColumnsAuto() {
+        this.headers.forEach((column) => {
+            const columnData = this.dataMap.get(column);
+            if (columnData && columnData.length > 0) {
+                const dataType: DataType = Number(columnData[0])
+                    ? "numeric"
+                    : "string";
+                const col = new Column(column, dataType);
+                const numberColumnData: number[] = columnData.map(
+                    (item: string) => parseFloat(item)
+                );
+                col.addRows(
+                    dataType === "numeric" ? numberColumnData : columnData
+                );
+                this.addColumn(col);
+            } else {
+                console.error(
+                    `Column ${column} has no data. Skipping this column.`
+                );
+            }
+        });
     }
 }
