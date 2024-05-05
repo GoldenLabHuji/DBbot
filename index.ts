31b961bd52dae1b880780aee8f66e4f08602bd7d
Shlomi Shitrit <akunh113@gmail.com>
2024-05-05 11:53:12 +0300

    remove testing

--- index.ts
+++ index.ts
@@ -3,11 +3,6 @@ const { DBbot, app: appClass } = require("./DBbot");
 const dbBot = new DBbot();
 const app = new appClass("./BotUI");
 
-// Testing
-// TODO: Remove this
-dbBot.loadFile("./data.csv");
-app.runBot(dbBot);
-
 module.exports = {
     dbBot,
     app,
