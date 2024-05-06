d2f9c017a0a26297d5d0310a88ea08b3b2e3387a
Shlomi Shitrit <akunh113@gmail.com>
2024-05-06 11:36:06 +0300

    add run demo command

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const { DBbot, app: appClass } = require("./DBbot");
 const dbBot = new DBbot();
 const app = new appClass("./BotUI");
+dbBot.loadFile("./data.csv");
+app.runBot(dbBot);
 module.exports = {
     dbBot,
     app,
--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ const { DBbot, app: appClass } = require("./DBbot");
 const dbBot = new DBbot();
 const app = new appClass("./BotUI");
 
+dbBot.loadFile("./data.csv");
+app.runBot(dbBot);
+
 module.exports = {
     dbBot,
     app,
