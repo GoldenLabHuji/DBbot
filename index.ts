f9ad88e6d17398cee2a5eccf6b9758207172a526
Shlomi Shitrit <akunh113@gmail.com>
2024-05-09 12:00:54 +0300

    add demo csv file

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const { DBbot, app: appClass } = require("./DBbot");
 const dbBot = new DBbot();
 const app = new appClass("./BotUI");
 
-dbBot.loadFile("./data.csv");
+dbBot.loadFile("./demo.csv");
 app.runBot(dbBot);
 
 module.exports = {
