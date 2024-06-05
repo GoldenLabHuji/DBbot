ab75a14cdd2051cbaf0a1120e42edce33a025f02
Shlomi Shitrit <akunh113@gmail.com>
2024-06-05 15:29:55 +0300

    change getOperator func

--- BotUI/src/app/general/utils.ts
+++ BotUI/src/app/general/utils.ts
@@ -1,11 +1,13 @@
-import {
-    greaterOperator,
-    lowerOperator,
-    equalOperator,
-    rangeOperator,
-    startWithOperator,
-    soundLikeOperator,
-} from "@/app/operators";
+// import {
+//     greaterOperator,
+//     lowerOperator,
+//     equalOperator,
+//     rangeOperator,
+//     startWithOperator,
+//     soundLikeOperator,
+// } from "@/app/operators";
+
+import { OPERATORS } from "@/app/operators/operators";
 
 export function isNumberArray(value: any): value is number[] {
     const isArray = Array.isArray(value);
@@ -17,20 +19,24 @@ export function isNumberArray(value: any): value is number[] {
 }
 
 export function getOperator(name: string) {
-    switch (name) {
-        case "Greater":
-            return greaterOperator;
-        case "Lower":
-            return lowerOperator;
-        case "Equal":
-            return equalOperator;
-        case "Range":
-            return rangeOperator;
-        case "SoundLike":
-            return soundLikeOperator;
-        case "StartWith":
-            return startWithOperator;
-        default:
-            throw new Error("Invalid operator name");
-    }
+    return OPERATORS[name as keyof typeof OPERATORS];
 }
+
+// export function getOperator(name: string) {
+//     switch (name) {
+//         case "Greater":
+//             return greaterOperator;
+//         case "Lower":
+//             return lowerOperator;
+//         case "Equal":
+//             return equalOperator;
+//         case "Range":
+//             return rangeOperator;
+//         case "SoundLike":
+//             return soundLikeOperator;
+//         case "StartWith":
+//             return startWithOperator;
+//         default:
+//             throw new Error("Invalid operator name");
+//     }
+// }
