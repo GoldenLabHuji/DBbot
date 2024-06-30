30d595db71788c5097d67fea6a1dc9cbe4af4576
Shlomi Shitrit <akunh113@gmail.com>
2024-06-30 15:30:52 +0300

    update demo file and added some comments

--- scripts/demo.ts
+++ scripts/demo.ts
@@ -1,54 +1,67 @@
 import { dbBot, app } from "../index";
-import { AddCustomOperatorParams } from "../DBbot/general/types";
+
+/////////////////
+// set details //
+/////////////////
 
 const details = {
-    name: "SW Database",
-    description: "A database of Star Wars characters",
-};
-
-const messages = {
-    welcomeMessage: "Welcome to the Star Wars Characters database",
-    attributeMessage:
-        "Here is a list of attributes you can query from the Star Wars Characters database:",
-    descriptionMessage:
-        "The Star Wars Characters database contains information about characters from the Star Wars universe",
-    exampleMessage:
-        "Get all characters that start with 'B' and end with 'X' with a custom operator",
-    operatorMessage: "Choose a Star Wars operator",
-    errorMessage: "what???",
+    name: "Demo database name",
+    description: "Demo database description",
 };
 
 dbBot.details = details;
-dbBot.messages = messages;
 
-const startWithBAndEndWithXOperator = {
-    name: "startWithBAndEndWithX",
-    customFunction: function (inputValue: string, endChar: string) {
-        return (
-            inputValue.toLowerCase().startsWith("b") &&
-            inputValue.toLowerCase().endsWith(endChar)
-        );
-    },
-    dataType: "string",
-    message:
-        "This operator Check if the input value starts with 'B' and ends with the given character",
-    params: [
-        {
-            name: "InputValue",
-            dataType: "string",
-        },
-        {
-            name: "endChar",
-            dataType: "string",
-            message: "Enter the character to end with:",
-        },
-    ],
+/////////////////////////
+// set custom messages //
+/////////////////////////
+
+const messages = {
+    attributeMessage: "Demo messages for attributes",
+    operatorMessage: "Demo message for operators",
+    errorMessage: "Demo error message",
+    resultMessage: "Demo result message",
 };
 
-dbBot.addCustomOperator(
-    startWithBAndEndWithXOperator as AddCustomOperatorParams
-);
+dbBot.customMessages = messages;
 
-dbBot.loadFile("./sw_characters.csv");
+///////////////
+// set slots //
+///////////////
+
+// welcome slot
+const welcomeMessages = [];
+
+const welcomeFirstMsg = "Demo welcome slot first message";
+
+welcomeMessages.push(welcomeFirstMsg);
+
+const welcomeSecondMsg = "Demo welcome slot second message";
+
+welcomeMessages.push(welcomeSecondMsg);
+
+// operator slot
+const operatorMessages = [];
+
+const operatorFirstMsg = "Demo operator slot first message";
+
+operatorMessages.push(operatorFirstMsg);
+
+const operatorSecondMsg = "Demo operator slot second message";
+
+operatorMessages.push(operatorSecondMsg);
+
+const slots = {
+    welcomeSlot: welcomeMessages,
+    operatorSlot: operatorMessages,
+};
+
+dbBot.slots = slots;
+
+///////////////////
+// load csv file //
+//   & run bot   //
+///////////////////
+
+dbBot.loadFile("./pokemon.csv");
 
 app.runBot(dbBot);
