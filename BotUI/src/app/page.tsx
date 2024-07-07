0dad2e97b5983ffaa6fd6f15324d3d81bacad5fa
Shlomi Shitrit <akunh113@gmail.com>
2024-07-07 16:02:14 +0300

    change method of storing bot data because of size

--- BotUI/src/app/page.tsx
+++ BotUI/src/app/page.tsx
@@ -1,6 +1,18 @@
 import Chat from "@/app/components/Chat";
+import fs from "fs";
 
 export default function Home() {
+    const path = process.env.DB_BOT_FILE;
+    if (path) {
+        fs.readFile(path, "utf8", (err, data) => {
+            if (err) {
+                console.error("Failed to read bot data file:", err);
+            } else {
+                console.log("data", JSON.parse(data));
+                process.env.DB_BOT = data;
+            }
+        });
+    }
     const bot = JSON.parse(process.env.DB_BOT as string);
 
     return <Chat bot={bot} />;
